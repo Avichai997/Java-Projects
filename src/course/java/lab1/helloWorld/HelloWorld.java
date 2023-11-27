8744b2115fbe196001092e4646cfb14812a47e1a
Avichai Iluz <Avichai997@gmail.com>
2023-11-27 23:26:29 +0200

    add jar

--- src/course/java/lab1/helloWorld/HelloWorld.java
+++ src/course/java/lab1/helloWorld/HelloWorld.java
@@ -1,7 +1,20 @@
 package course.java.lab1.helloWorld;
 
+import course.java.lab1.data.Worker;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Worker[] workers = new Worker[10];
+
+        for (int i = 0; i < workers.length; i++) {
+            workers[i] = new Worker();
+            workers[i].setAge(25 + i);
+        }
+
+        for (int i = 0; i < workers.length; i++) {
+            System.out.println(workers[i].getAge());
+        }
     }
 }
